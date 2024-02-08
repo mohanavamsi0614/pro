3d32c1b5e23b08846254d1f5aaad0d6d3d59ddf4
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-02-08 14:49:52 +0530

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   return (
       <>
        <nav>
-        <NavLink to='/'><img src={kal}/></NavLink>
-        <NavLink to='/about'><p>About</p></NavLink>
-        <NavLink to="/Contact"><p>Contact</p></NavLink>
+        <NavLink to='/'><img src={kal} className='img'/></NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to="/form">form</NavLink>
       </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/form" element={<Contact />} />
         </Routes>
 
     </>
